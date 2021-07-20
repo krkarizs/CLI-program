dc997d2ed09dec8ede51ba784f21c69ad16ece57
krkarizs <sevenke@gmail.com>
2021-07-20 13:22:13 +0300

    insert commands added

--- src/query.py
+++ src/query.py
@@ -2,26 +2,35 @@ import psycopg2
 from config import config
 #import Nikon py
 
-def connect():
+def insert():
+    #Insert the full name into the agent table (if it is not there yet) - it is possible to add the number of work hours to this table
+    add_name = "INSERT INTO agent (name) VALUES (%s) RETURNING id;"
+    #Insert the project name into the project table (if it is not there yet) - it is possible to add the total hours spent on this project
+    add_project = "INSERT INTO project (name) VALUES (%s) RETURNING id;"
+    #Insert the login date/time, the logout date/time and the comment into the third table
+    add_time = "INSERT INTO logs (logintime, logouttime, metadata) VALUES (%s, %s, %s)"
+
+
     con = None
     try:
         #Connect to an existing databese
         con = psycopg2.connect(**config())
         #Open a cursor to perform database operations
         cursor = con.cursor()
-        #Database opeartions with SQL specified in separate functions
+        #Database opeartions with SQL
+        cursor.execute(add_name, fullname)
+        person_id = cursor.fetchone()[0]
 
+        cursor.execute(add_project, projectname)
+        project_id = cursor.fetchone()[0]
 
+        cursor.execute(add_time, logintime, logouttime, comment)
         #Close the connection with the database
+        con.commit()
         cursor.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
+        con.rollback()
     finally:
         if con is not None:
             con.close()
-
-def insert(cursor, arvot):
-    cursor.execute(arvot)
-
-if __name__ == '__main__':
-    connect()
