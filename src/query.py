7386f2f938becea0b577679bfda267d14744346b
gitmikko <mikontreeniacco@gmail.com>
2021-07-20 22:24:48 +0300

    mikon vika sekoilu

--- src/query.py
+++ src/query.py
@@ -2,10 +2,6 @@ import psycopg2
 from config import config
 import main
  
-""" fullname = "Niko Niinimaki"
-projectname = "Python"
-logdata = ("2000-12-22 01:00", "2000-12-24 02:00", 35, "Frontend project") """
- 
 def insert(fullname, projectname, logdata):
     #Insert the full name into the agent table (if it is not there yet) - it is possible to add the number of work hours to this table
     add_name = "INSERT INTO agent (nimi) VALUES (%s) RETURNING id;"
@@ -90,11 +86,6 @@ def check_if_project_exist(projectname):
     
 if __name__ == '__main__':
     main.finish_task()
-    #print(f"{main.login.starttime[0]}-{main.login.starttime[1]}-{main.login.starttime[2]} {main.login.starttime[3]}:{main.login.starttime[4]}")
-    #print(f"{main.login.endtime[0]}-{main.login.endtime[1]}-{main.login.endtime[2]} {main.login.endtime[3]}:{main.login.endtime[4]}")
-    #print(main.login.metadata)
-    #print(main.login.name)
-    #print(main.login.project)
     logdata_starttime = f"{main.login.starttime[0]}-{main.login.starttime[1]}-{main.login.starttime[2]} {main.login.starttime[3]}:{main.login.starttime[4]}"
     logdata_endtime = f"{main.login.endtime[0]}-{main.login.endtime[1]}-{main.login.endtime[2]} {main.login.endtime[3]}:{main.login.endtime[4]}" 
     insert(main.login.name, main.login.project, (logdata_starttime, logdata_endtime, main.login.totalhours, main.login.metadata))
