5cb2034e8c60dbd6e9ad99ab37c05d4403b08d35
krkarizs <sevenke@gmail.com>
2021-07-21 12:48:30 +0300

    new pip install commands added to the startup script

--- terraform/startup.sh
+++ terraform/startup.sh
@@ -3,8 +3,11 @@
     sudo apt-get -y install python3-pip
     #download the required modules (for cron but also for the email and the SQL)
     pip3 install python-crontab
-    pip install configparser
-    pip install psycopg2
+    pip3 install configparser
+    pip3 install psycopg2
+    pip3 install secure-smtplib
+    pip3 install ssl
+    pip3 install email-to
     #upload the python file tht modifies the crontab (we can put them in a bucket?)
     gsutil cp -r gs://YOUR-BUCKET-NAME/file.py .
     #upload the python file that runs the queries and sends the email
